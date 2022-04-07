6d9ca44612e501b0a942534a974b14ce0dd3713b
unknown <faicel.ghozzi@esprit.tn>
2022-04-07 23:56:08 +0100

    added user model

--- src/main/java/com/example/user_management/model/User.java
+++ src/main/java/com/example/user_management/model/User.java
@@ -1,2 +1,111 @@
-package com.example.user_management.model;public class User {
+package com.example.user_management.model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotNull
+    private String firstname;
+
+    @NotNull
+    private String lastname;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private LocalDate birthday;
+
+    @NotNull
+    private LocalDate createdAt;
+
+
+    public User() {
+    }
+
+
+    public User(Integer id, String firstname, String lastname, String email, LocalDate birthday, LocalDate createdAt) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.birthday = birthday;
+        this.createdAt = createdAt;
+    }
+
+    public User(String firstname, String lastname, String email, LocalDate birthday, LocalDate createdAt) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.birthday = birthday;
+        this.createdAt = createdAt;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return this.lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthday() {
+        return this.birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public LocalDate getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", firstname='" + getFirstname() + "'" +
+                ", lastname='" + getLastname() + "'" +
+                ", email='" + getEmail() + "'" +
+                ", birthday='" + getBirthday() + "'" +
+                ", createdAt='" + getCreatedAt() + "'" +
+                "}";
+    }
 }
